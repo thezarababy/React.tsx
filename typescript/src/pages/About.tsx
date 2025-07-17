966e10e69370859bf5b68153162795d0c8f367b1
thezarababy <jyokoduwa@gmail.com>
2025-07-17 11:58:47 +0100

    Api consumption 2

--- typescript/src/pages/About.tsx
+++ typescript/src/pages/About.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+//step 1: define data types 
+interface iUser{
+  id: number
+  name: string
+  username:string
+  email: string
+}
 
 const About = () => {
+
+//step 2 functional components
+  const[user,setUser]=useState<iUser[]>([])
+  const[loading,setLoading]=useState<boolean>(true)
+
+  //step 3 fetch Api
+
+  const fetchUsers= async()=>{
+    try{
+      const res= await axios.get("https://jsonplaceholder.typicode.com/users")
+      setUser(res.data)
+    }
+    catch(error){
+      console.error("failed", error)
+    }
+    finally{
+      setLoading(false)
+    }
+   
+    
+  }
+  useEffect(()=>{
+    fetchUsers(),([])
+  })
+  const getInitials = (name: string): string => {
+  return name
+    .split(" ")              // split full name by space
+    .map((n) => n[0])        // take first letter of each word
+    .join("")                // combine into a string
+    .toUpperCase();          // make it uppercase
+};
+
   return (
-    <div>
-      This is the about
+    <div className='max-w-[1280px] mx-auto'>
+      <h1 className='text-3xl font-bold text-center py-11 text-gray-800'> All Users</h1>
+      <div className=' flex justify-center items-center'>
+           <button onClick={fetchUsers} disabled={loading} className='bg-gray-800 hover:bg-gray-700 text-white font-semibold px-6 py-4'>{loading? "a min pls...":"Fetch all users"}
+
+      </button>
+      </div>
+     
+      <section className='flex flex-wrap gap-7 my-6'>
+
+        {user?.map((item:iUser,i:number)=>(
+            <main className='flex items-center bg-black text-white p-5 w-100'  key={i}>
+              <div className='bg-pink-500 rounded-full mr-4 text-purple-900 p-6 text-2xl'>{getInitials(item.name)}</div>
+              <div className='w-80'>
+                <h1>name : {item.name}</h1>
+                <p className=''>username : {item.username}</p>
+                <h1>email : {item.email}</h1>
+              </div>
+
+            </main>
+        ))}
+
+      </section>
+     
     </div>
   )
 }
--- typescript/src/pages/Home.tsx
+++ typescript/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 interface allUsers{
     _id:string;
     username: string;
@@ -10,20 +11,41 @@ interface allUsers{
 }
 
 const Home = () => {
+    const [getUser, setGetUser]= useState <allUsers[]>([]);
+    const getMyUser= async()=>{
+        const res= await axios.get('https://user-data-ci61.onrender.com/user/viewalluser');
+        setGetUser(res.data.data)
+
+    }
+    useEffect(()=>{
+        getMyUser(),([])
+    })
+    
 
   return (
-    <div >
-        <h1>Subscribers</h1>
-        <section>
-            <div>
-                A
-            </div>
-            <div>
-                name:joy okoduwa
-                username:jyokoduwa@gmail.com
-                age:25
-                bio:i love coding and eating. 
-            </div>
+    <div className='max-w-[1280px] mx-auto '>
+        <h1 className='text-6xl font-bold text-center py-11 text-gray-800'>Subscribers</h1>
+        <section  className='flex flex-wrap gap-7'>
+            {getUser?.map((item:allUsers,i:number)=>(
+                <main className='flex items-center  shadow-lg bg-white w-100  p-4 hover:transition-all hover:translate-2.5' key={i}>
+                    <div className='bg-gray-800 text-white text-5xl p-7 rounded-full mr-3 '>
+                    {item.username.charAt(0)}
+                    </div>
+                    <div className='w-80 '>
+                    <h2> Name: {item.username}</h2>
+                    <p >Email: {item.email}</p>
+                    <p>Age: {item.age}</p>
+                    <p> Adreess: {item.address}</p>
+                    <p >Bio: {item.bio}</p>
+                   
+                </div>
+                </main>
+            ))}
+            
+            
+            
+            
+           
         </section>
     </div>
   )
